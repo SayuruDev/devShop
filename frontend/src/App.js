3be8b393eee5de7ba0d6eb97eca5ab802f7ab5e0
Sayuru Dev <mailtosayuru@gmail.com>
2025-05-05 02:00:22 -0400

    redirect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
 import { Container } from 'react-bootstrap';
- import Header from './components/Header';
- import Footer from './components/Footer';
- import HomeScreen from './screens/HomeScreen';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-
- 
- const App = () => {
-   return (
-     <>
-       <Header />
-       <main className='py-3'>
-         <Container>
-           <HomeScreen />
-         </Container>
-       </main>
-       <Footer />
-     </>
-   );
- };
- 
- export default App;
+export default App;
