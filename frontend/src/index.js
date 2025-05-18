b3cd44e184cbf246fad649f17c61851123c282d3
Sayuru Dev <mailtosayuru@gmail.com>
2025-05-18 03:26:35 -0400

    redux && loading && cart state

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,8 @@ import {
 } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import store from './store';
+import { Provider } from 'react-redux';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,8 +27,10 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
-reportWebVitals();  
+reportWebVitals();
