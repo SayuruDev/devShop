9e6969f796f168b776dd439dc7992e68a559c35a
Sayuru Dev <mailtosayuru@gmail.com>
2025-05-20 02:06:42 -0400

    cart backend

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,8 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice';
 import cartSliceReducer from './slices/cartSlice';
 
-
-
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
@@ -11,6 +9,7 @@ const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
 export default store;
